347fbc8c69433c67df41f93e60f6b5dc35b32a46
hugo <hugohuang1111@gmail.com>
2021-03-10 00:07:44 +0800

    fix action

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -18,6 +18,7 @@ module.exports = {
             src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
         }, ``]
     ],
+    base:'/Blog/',
     theme: '@vuepress/theme-blog',
     themeConfig: {
         sitemap: {
